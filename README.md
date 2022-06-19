5586e54e77d8f977e0668c2eb340d0d58e9f76bf
Abd El-Twab M. Fakhry <abdeltwab.m.fakhry@protonmail.com>
2022-06-19 11:51:00 +0200

    Add Makefile

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
-# Resume
+# Resume for Software Engineers
 
 A single-page, one-column resume for Software Engineers. It uses the base latex templates and fonts to provide ease of use and installation when trying to update the resume. The different sections are clearly documented and custom commands are used to provide consistent formatting. The three main sections in the resume are education, experience, and projects.
 
----
-
-### Preview
+## Preview
 
 ![RESUME PREVIEW](Resume_Preview.png)
 
----
+## Build
 
-### Licensed under the [MIT License](LICENSE)
+This resume is compiled using the pdflatex engine, so make sure it is present.
+
+```bash
+git clone https://github.com/AbdeltwabMF/resume.git
+cd resume
+make all clean
+```
+
+## Licensed under the [MIT License](LICENSE)
--- auto/abdeltwabmf_resume.el
+++ auto/abdeltwabmf_resume.el
@@ -1,17 +1,17 @@
 (TeX-add-style-hook
- "amf_resume"
+ "abdeltwabmf_resume"
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("article" "letterpaper" "11pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("fullpage" "empty") ("hyperref" "hidelinks") ("babel" "english") ("xcolor" "dvipsnames")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
